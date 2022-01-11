c2d9998a50149bdb37f133e41f4650d73aff5dee
PauloBaronista <paulo@rpm.com.br>
2022-01-11 18:41:50 -0300

    pixel

--- application/views/html_footer.php
+++ application/views/html_footer.php
@@ -11,6 +11,23 @@ echo script_tag('assets/js/bootstrap.min.js');
     <a href="https://online.crm.anapro.com.br/WebCRMService/Pages/chat/cliente/ChatClienteWhatsAppEntrada.aspx?conta=vTjaeIT3TpI1&keyIntegradora=29F2D83A-29CF-4359-B1AC-40CE6FA19E70&keyAgencia=29F2D83A-29CF-4359-B1AC-40CE6FA19E70&strDir=coelhodafonseca&campanha=srQjaQEk5sU1&canal=9B19BZMXEwI1&produto=PdfZ9LzmHVY1&strmidia=Hotsite+Empreendimento&strpeca=&usuarioEmail=&strgrupopeca=&strcampanhapeca=&nome=&email=&telefoneDDD=&telefone=&strTexto=&keyexterno=&urlep=&urlcp=&urlca=&urlat=&strMostrarTopo=true&strAutoSubmit=true&strUsarDadosAnteriores=true&emailobrigatorio=true&telefoneobrigatorio=false&texto=" target="_blank" class="whats">Whatsapp</a>
 </div>
 
+<!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '210750254603166');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=210750254603166&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-VFN50J4KW6"></script>
